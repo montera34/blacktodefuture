e6df37b1366171f34053f7c8809d9820f26ce398
numeroteca <pablo@basurama.org>
2016-12-06 01:12:45 +0100

    changes look to dark. Adds banner image on top

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
 <head profile="http://gmpg.org/xfn/11">
-	<title>Hace 10 a&ntilde;os gastaban las tarjetas black en...</title>
+	<title>Tarjetas Black to de Future. Hace 10 a&ntilde;os gastaban las tarjetas black en...</title>
 	<meta http-equiv="content-type" content="text/html"/>
 	<meta name="description" content="quizás no necesites una web" />
 	<meta charset="utf-8">
@@ -15,14 +15,14 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:description" content="¿Qué estaban gastando justo hace 10 años los de las tarjetas black?" />
 	<meta content="corrupción,caja marid,bankia" />
-	<meta property="og:url" content="http://lab.montera34.com" /> 
+	<meta property="og:url" content="http://lab.montera34.com/blacktodefuture" /> 
 	<!-- twitter meta -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@montera34">
 	<meta name="twitter:title" content="Hace 10 años gastaban las tarjetas black en..." />
 	<meta name="twitter:description" content="¿Qué estaban gastando justo hace 10 años los de las tarjetas black?" />
 	<meta name="twitter:creator" content="@montera34">
-	<meta name="twitter:image:src" content="pendiente" />
+	<meta name="twitter:image:src" content="pendiente" /> 	<!-- TODO: pending image -->
 	<meta property="twitter:account_id" content="137677992" />
 	
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
@@ -103,8 +103,15 @@ if ($dayofweek == 0 ) {
 <div class="container main-container">
 	<div class="row">
 		<div class="share pull-right">
-			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://lab.montera34.com" data-text="¿Qué estaban gastando justo hace 10 años los de las tarjetas black?" data-via="montera34" data-lang="es">Twittear</a>
+			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://lab.montera34.com/blacktodefuture" data-text="¿Qué estaban gastando justo hace 10 años los de las tarjetas black?" data-via="montera34" data-lang="es">Twittear</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		</div>
+	</div>
+	<div class="row">
+		<img src="img/banner-header-black-to-the-future-c.jpg" class="img-responsive" alt="Responsive image">
+		<div class="col-md-4 col-md-offset-4">
+			<p class="clear">Sigue en Twitter los gastos en riguroso diferido:</p>
+			<a class="btn btn-dark btn-lg btn-block" href="https://twitter.com/BlacktodeFuture">@BlacktodeFuture</a>
 		</div>
 	</div>
 	<div class="row">
@@ -148,9 +155,10 @@ if ($dayofweek == 0 ) {
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		Si quieres saber m&aacute;s de estos gastos puedes consultarlos gr&aacute;ficamente en <a href="http://numeroteca.org/tarjetasblack/">numeroteca.org/tarjetasblack</a>
-		<a class="btn btn-info pull-right" href="https://github.com/montera34/habiaunavez" role="button">Colabora <span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></a>
+	<div class="row clear">
+		<p>Si quieres saber m&aacute;s de estos gastos puedes consultarlos <a href="http://numeroteca.org/tarjetasblack/">gr&aacute;ficamente en esta visualizaci&oacute;n.</a></p>
+		<p>Este un experimento de <a href="https://montera34.com">montera34.com</a>. Aportaron ideas <a href="https://twitter.com/martgnz">@guillelamb</a>y <a href="https://twitter.com/martgnz">@marting</a>.</p>
+		<a class="btn btn-dark pull-right" href="https://github.com/montera34/habiaunavez">Colabora <span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></a>
 	</div>
 </div>	
 </body>
